a03c80d495ba952573ef4477c3057d682c14f814
James Launder <launderj@student.unimelb.edu.au>
2021-07-04 20:04:02 +1000

    added help and more modularity

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,7 +1,8 @@
 module.exports = {
 	name: 'kick',
 	description: 'Tag a member and kick them (but not really).',
-	execute(message) {
+	guildOnly: true,
+	execute(message, args) {
 		if (!message.mentions.users.size) {
 			return message.reply('you need to tag a user in order to kick them!');
 		}
